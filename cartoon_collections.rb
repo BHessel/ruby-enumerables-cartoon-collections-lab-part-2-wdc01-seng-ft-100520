ae7778b9b6ee9b9ef055d4d1d010a947fafb922c
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-17 18:01:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find {|match| match == valid_calls}
+  valid_calls.find {|match| match == planeteer_calls}
   
   # Does ANYthing in valid_calls MATCH ANYthing in planeteer_calls?
   #If so, return first match -- .find seems to do this automatically
