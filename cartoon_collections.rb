f4f273fc2797f6ec9c676da97d82a6a8cb1ae956
Benjamin Hessel <hesselfitness@gmail.com>
2020-09-17 18:06:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,6 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  #valid_calls.find {|i| planeteer_calls.include? (i)}
-  valid_calls.find do |i|
-    planeteer_calls.include? (i)
-  end
+  valid_calls.find {|i| planeteer_calls.include? (i)}
+  
 end
